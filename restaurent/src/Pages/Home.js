4ff852f566a0a9fd6d71e3521f3f541d07301a37
Sakshi Maheshwari <sakshicoder800@gmail.com>
2024-02-16 12:46:47 +0530

    create routes

---
+++ restaurent/src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../Components/Layouts/Layout'
+
+const Home = () => {
+  return (
+    <Layout>
+      Home Page
+    </Layout>
+  )
+}
+
+export default Home
---
+++ restaurent/src/Pages/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../Components/Layouts/Layout'
+
+const Menu = () => {
+  return (
+    <Layout>
+      Menu
+    </Layout>
+  )
+}
+
+export default Menu
