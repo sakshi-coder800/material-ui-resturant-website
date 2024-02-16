1ffab490f570e8dd9a8159887311a6bd383ab164
Sakshi Maheshwari <sakshicoder800@gmail.com>
2024-02-16 13:20:18 +0530

    create navbar for large screen

--- restaurent/src/Components/Layouts/Header.js
+++ restaurent/src/Components/Layouts/Header.js
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import { AppBar, Box, Toolbar, Typography } from '@mui/material'
+import { NavLink} from 'react-router-dom';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <Box>
+      <AppBar  component={"nav"} sx={{bgcolor:"black"}}>
+        <Toolbar >
+          <Typography color={'goldenrod'} variant='h6' component={'div'} sx={{flexGrow:"1"}}> <FastfoodIcon/> My Restaurent </Typography>
+        <Box sx={{display : {xs:"none", sm:"block"}}}>
+           <ul className='Navigation-menu'>
+            <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/menu">Menu</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+           </ul>
+        </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 
